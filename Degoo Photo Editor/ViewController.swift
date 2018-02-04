99659ca9729960150a931834f20428965348f5b5
Jovito Royeca <jovit.royeca@gmail.com>
2018-02-05 01:37:40 +0800

    feat: Can now access the camera and photo library.

--- Degoo Photo Editor/ViewController.swift
+++ Degoo Photo Editor/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import PhotoEditorSDK
 
 class ViewController: UIViewController {
 
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        presentCameraViewController()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Custom methods
+    
+    func presentCameraViewController() {
+        let configuration = buildConfiguration()
+        let cameraViewController = CameraViewController(configuration: configuration)
+        
+        cameraViewController.dataCompletionBlock = { [unowned cameraViewController] data in
+            if let data = data {
+                let photo = Photo(data: data)
+                cameraViewController.present(self.createPhotoEditViewController(with: photo), animated: true, completion: nil)
+            }
+        }
+        
+        navigationController?.present(cameraViewController, animated: true, completion: nil)
+    }
+    
+    func createPhotoEditViewController(with photo: Photo) -> PhotoEditViewController {
+        let configuration = buildConfiguration()
+        var menuItems = PhotoEditMenuItem.defaultItems
+        menuItems.removeLast() // Remove last menu item ('Magic')
+        
+        // Create a photo edit view controller
+        let photoEditViewController = PhotoEditViewController(photoAsset: photo, configuration: configuration, menuItems: menuItems)
+        photoEditViewController.delegate = self
+        
+        return photoEditViewController
+    }
+    
+    func buildConfiguration() -> Configuration {
+        let configuration = Configuration() { builder in
+            // Configure camera
+            builder.configureCameraViewController() { options in
+                // Just enable Photos
+                options.allowedRecordingModes = [.photo]
+            }
+        }
+        
+        return configuration
+    }
 }
+
+// MARK: PhotoEditViewControllerDelegate
+extension ViewController : PhotoEditViewControllerDelegate {
+    func photoEditViewController(_ photoEditViewController: PhotoEditViewController, didSave image: UIImage, and data: Data) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func photoEditViewControllerDidFailToGeneratePhoto(_ photoEditViewController: PhotoEditViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func photoEditViewControllerDidCancel(_ photoEditViewController: PhotoEditViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
 
